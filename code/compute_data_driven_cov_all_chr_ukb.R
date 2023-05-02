ca61e1cc3269bac8f2382bf6cf955241388099e8
fmorgante <fabiom@clemson.edu>
2023-05-02 13:56:20 -0400

    Add step to compute data-driven matrices

---
+++ code/compute_data_driven_cov_all_chr_ukb.R
@@ -0,0 +1,87 @@
+###Load libraries needed
+library(optparse)
+library(mashr)
+
+###Function needed
+is_strong <- function(x, thresh){
+  strong <- any(abs(x) > thresh)
+  return(strong)
+}
+
+###Parse arguments
+parser <- OptionParser()
+parser <- add_option(parser, c("--sumstats"), type="character")
+parser <- add_option(parser, c("--residual_cov"), type="character")
+parser <- add_option(parser, c("--strong_Z_thresh"), type="integer")
+parser <- add_option(parser, c("--n_PCs"), type="integer")
+parser <- add_option(parser, c("--flash_remove_singleton"), type="logical")
+parser <- add_option(parser, c("--ED_algorithm"), type="character", )
+parser <- add_option(parser, c("--data_id"), type="integer")
+outparse <- parse_args(parser)
+
+input <- outparse$sumstats
+residual_cov <- outparse$residual_cov
+strong_thresh <- outparse$strong_Z_thresh
+npcs <- outparse$n_PCs
+flash_remove_singleton <- outparse$flash_remove_singleton
+ED_algorithm <- outparse$ED_algorithm
+data_id <- outparse$data_id
+
+###Set seed
+set.seed(data_id)
+
+###Extract strong effects
+for(i in 1:22){
+  dat <- readRDS(paste0("../output/summary_statistics/", input,"_chr", i, "_sumstats_", data_id, ".rds"))
+  Z <- dat$Bhat/dat$Shat
+  strong <- which(apply(Z, 1, is_strong, strong_thresh))
+  
+  if(i==1){
+    Bhat_strong <- dat$Bhat[strong, ]
+    Shat_strong <- dat$Shat[strong, ]
+  } else {
+    Bhat_strong <- rbind(Bhat_strong, dat$Bhat[strong, ])
+    Shat_strong <- rbind(Shat_strong, dat$Shat[strong, ])
+  }
+}
+
+if(nrow(Bhat_strong) < 20){
+  stop("Too few strong effects with current strong_Z_thresh value.")
+}
+
+###Estimate data-driven prior
+if(residual_cov=="diagonal"){
+  V <- diag(ncol(Bhat_strong))
+} else {
+  V <- readRDS(paste0("../output/misc/", residual_cov, "_", data_id, ".rds"))
+}
+
+dat_mash <- mash_set_data(Bhat_strong, Shat_strong, V=cov2cor(V))
+
+##Compute data-driven matrices
+U_pca <- cov_pca(data=dat_mash, npc=npcs)
+U_flash_default <- cov_flash(data=dat_mash, factors="default", tag="default",
+                              remove_singleton=flash_remove_singleton, output_model=NULL)
+U_flash_nonneg <- cov_flash(data=dat_mash, factors="nonneg", tag="nonneg",
+                             remove_singleton=flash_remove_singleton, output_model=NULL)
+U_emp <- mr.mash.alpha:::cov_empirical(data=dat_mash)
+
+##De-noise data-driven matrices via extreme deconvolution
+U_datadriven <- c(U_pca, U_flash_default, U_flash_nonneg, list(BB=U_emp))
+if(ED_algorithm=="bovy"){
+  U_ed <- cov_ed(dat_mash, U_datadriven)
+} else if(ED_algorithm=="ted" || ED_algorithm=="ed"){
+  library(udr)
+  f0 <- ud_init(X = as.matrix(Bhat_strong/Shat_strong), V = V, U_scaled = list(), 
+                U_unconstrained = U_datadriven, n_rank1=0)
+  res <-ud_fit(f0, X = na.omit(f0$X), control = list(unconstrained.update = ED_algorithm, 
+                                                     resid.update = "none",
+                                                     maxiter=5000, tol.lik = 1e-3), 
+                                                     verbose=TRUE)
+
+U_ed <- lapply(res$U,"[[",2)
+}
+
+
+###Save file
+saveRDS(U_ed, file=paste0("../output/misc/", input, "_", ED_algorithm, "_data_driven_cov_", data_id, ".rds"))
