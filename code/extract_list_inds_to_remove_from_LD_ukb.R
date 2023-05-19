9f24f8e3aa08313afdef043c8a0936a2f577d43d
fmorgante <fabiom@clemson.edu>
2023-05-19 15:51:28 -0400

    Use R for this step

---
+++ code/extract_list_inds_to_remove_from_LD_ukb.R
@@ -0,0 +1,26 @@
+###Load libraries needed
+library(optparse)
+library(data.table)
+
+###Parse arguments
+parser <- OptionParser()
+parser <- add_option(parser, c("--geno_fam"), type="character")
+parser <- add_option(parser, c("--withdrawn_inds"), type="character")
+parser <- add_option(parser, c("--output"), type="character")
+outparse <- parse_args(parser)
+
+geno_dat <- outparse$geno_fam
+withdrawn_inds <- outparse$withdrawn_inds
+output <- outparse$output
+
+###Read in the data
+options(datatable.fread.datatable=FALSE)
+
+geno_fam <- fread(geno_dat, showProgress=FALSE)[, 1:2]
+withdr <- fread(withdrawn_inds, showProgress=FALSE)
+
+###Merge the data
+total <- rbind(geno_fam, withdr)
+total <- total[!duplicated(total), ]
+
+fwrite(total, file=output, row.names=FALSE, col.names=FALSE, sep=" ", quote=FALSE, showProgress=FALSE)
