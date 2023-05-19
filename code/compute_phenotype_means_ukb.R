8733a25a003d35562b9a08532587f2b289b69373
fmorgante <fabiom@clemson.edu>
2023-05-19 16:10:05 -0400

    Fix bugs

--- code/compute_phenotype_means_ukb.R
+++ code/compute_phenotype_means_ukb.R
@@ -1,14 +1,12 @@
 ###Load libraries needed
 library(optparse)
 library(data.table)
-library(bigsnpr)
-library(bigstatsr)
 
 ###Parse arguments
 parser <- OptionParser()
 parser <- add_option(parser, c("--pheno"), type="character")
 parser <- add_option(parser, c("--test_ids"), type="character")
-parser <- add_option(parser, c("--outprefix"), type="character")
+parser <- add_option(parser, c("--output"), type="character")
 parser <- add_option(parser, c("--seed"), type="integer")
 outparse <- parse_args(parser)
 
@@ -22,7 +20,7 @@ set.seed(seed)
 
 ###Read in data
 options(datatable.fread.datatable=FALSE)
-test_ids <- fread(test_ids, showProgress=FALSE)
+test_ids <- fread(test_ids, showProgress=FALSE, header=FALSE)
 pheno <- readRDS(pheno_dat,)$Y
 training_inds_pheno <- which(!(rownames(pheno) %in% test_ids[,2])) ##Get only training individuals
 pheno <- pheno[training_inds_pheno, ]
