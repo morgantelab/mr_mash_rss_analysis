aa0b954299e07119bd2bb2e62e852a3b1e41d113
fmorgante <fabiom@clemson.edu>
2023-06-21 08:36:24 -0400

    Add code and scripts for some steps of the BP analysis

---
+++ code/compute_sumstats_BP_ukb.R
@@ -0,0 +1,133 @@
+###Load libraries needed
+library(optparse)
+library(bigsnpr)
+library(RhpcBLASctl)
+
+###Function to compute summary stats
+compute_univariate_sumstats_bigsnp <- function(geno_obj, Y, Z=NULL, chr=0, standardize=FALSE, 
+                                               impute_missing=TRUE, train_inds=NULL,
+                                               standardize.response=FALSE, mc.cores=1){
+  
+  if(impute_missing){
+    X <- bigsnpr::snp_fastImputeSimple(geno_obj$genotypes, method = "mean2", ncores = mc.cores)
+  } else {
+    X <- geno_obj$genotypes
+  }
+
+  r <- ncol(Y)
+  n <- nrow(Y)
+  p <- geno_obj$genotypes$ncol
+  
+  variable_names <- geno_obj$map$marker.ID
+  response_names <- colnames(Y)
+  
+  if(is.null(train_inds)){
+    row_inds <- seq_len(n)
+  } else {
+    row_inds <- train_inds
+    Y <- Y[row_inds, ]
+    if(!is.null(Z)){
+      Z <- Z[row_inds, ]
+    }
+  }
+  
+  if(standardize.response){
+    Y <- mr.mash.alpa::scale_fast2(Y, scale=TRUE)$M
+  }
+  
+  if(chr=="0"){
+    col_inds <- seq_len(p)
+  } else {
+    col_inds <- which(geno_obj$map$chromosome==chr)
+  }
+  
+  linreg <- function(i, X, Y, Z, cols, rows){
+    
+    fit <- bigstatsr::big_univLinReg(X=X, y=Y[, i], covar.train=Z,
+                                     ind.col=cols, ind.train=rows,  ncores = 1)
+    bhat <- fit$estim
+    shat <- fit$std.err
+    
+    return(list(bhat=bhat, shat=shat))
+  }
+  
+  if(mc.cores>1){
+    cl <- parallel::makeCluster(mc.cores)
+    out <- parallel::parLapply(cl, 1:r, linreg, X, Y, as.matrix(Z), col_inds, row_inds)
+    parallel::stopCluster(cl)
+  } else {
+    out <- lapply(1:r, linreg, X, Y, as.matrix(Z), col_inds, row_inds)
+  }
+  
+  Bhat <- sapply(out,"[[","bhat")
+  Shat <- sapply(out,"[[","shat")
+  colnames(Bhat) <- colnames(Shat) <- response_names
+  rownames(Bhat) <- rownames(Shat) <- variable_names[col_inds]
+  
+  ###Put coefficients and ses on the standardized X scale   
+  if(standardize){
+    sds <- sqrt(bigstatsr::big_colstats(X, ind.col=col_inds)$var)
+  	
+    Bhat <- Bhat*sds
+    Shat <- Shat*sds
+  }
+  
+  return(list(Bhat=Bhat, Shat=Shat))
+}
+
+###Parse arguments
+parser <- OptionParser()
+parser <- add_option(parser, c("--fold"), type="integer")
+parser <- add_option(parser, c("--chr"), type="character")
+parser <- add_option(parser, c("--standardize"), type="logical")
+parser <- add_option(parser, c("--ncores"), type="integer")
+parser <- add_option(parser, c("--impute_missing"), type="logical")
+outparse <- parse_args(parser)
+
+chr <- outparse$chr
+fold <- outparse$fold
+standardize <- outparse$standardize
+ncores <- outparse$ncores
+impute_missing <- outparse$impute_missing
+
+###Set MKL threads
+RhpcBLASctl::blas_set_num_threads(1)
+
+###Set seed
+set.seed(1)
+
+###
+if(chr %in% c("1", "2", "3", "4", "5" , "6", "7", "8", "9")){
+  chr <- paste0("0", chr)
+}
+
+###Read in data
+pheno_dat <- readRDS("../data/phenotypes/ukb_tiezzi_cleaned_BP_covar_pheno.rds")
+rownames(pheno_dat) <- pheno_dat$ID
+pheno_dat$ID <- NULL
+ukb_data_location <- "/data2/morgante_lab/data/ukbiobank/genotypes/imputed/"
+ids <- bigreadr::fread2(paste0(ukb_data_location, "ukb22828_c1_b0_v3_s487271.sample"))
+ids <- ids[-1, ]
+
+###Order phenotype and covariate data according to genotype data
+ids <- ids[which(as.character(ids$ID_2) %in% rownames(pheno_dat)), "ID_2"]
+pheno_dat <- pheno_dat[as.character(ids), ]
+
+###Get training individuals indexes
+train_inds <- which(pheno_dat$fold != fold)
+
+###Load genotype data
+geno <- snp_attach("/scratch1/fabiom/ukb_geno_imp_HM3_tiezzi.rds")
+
+###Compute summary stats
+out <- compute_univariate_sumstats_bigsnp(geno_obj=geno, Y=pheno_dat[, 1:3], Z=pheno_dat[, 4:25],
+                                          train_inds=train_inds, chr=chr, standardize=standardize,
+                                          impute_missing=impute_missing, standardize.response=FALSE, 
+                                          mc.cores=ncores)
+
+if(chr=="0"){
+  chr <- "All"
+}
+
+saveRDS(out, file=paste0("../output/summary_statistics/ukb_tiezzi_BP_chr", chr, "_sumstats_", fold, ".rds"))
+
