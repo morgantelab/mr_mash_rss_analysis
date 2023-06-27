66954dc9fa2b359ea56d29721cc18c6dcd57a432
fmorgante <fabiom@clemson.edu>
2023-06-27 19:18:16 -0400

    Use BMI instead of PP and unscaled phenotypes

--- code/prepare_tiezzi_BP_pheno_ukb.R
+++ code/prepare_tiezzi_BP_pheno_ukb.R
@@ -5,7 +5,7 @@ library(data.table)
 set.seed(1)
 
 ###Load data cleaned up by Francesco for the GxE project and the ids of the imputed genotype data
-load("/data2/morgante_lab/ukbiobank_projects/GxE/datasets/data3_20230511.RData")
+load("/data2/morgante_lab/ukbiobank_projects/GxE/datasets/data2_20230413.RData")
 load("/data2/morgante_lab/ukbiobank_projects/GxE/G/eigenG_20230413.RData")
 ids <- fread("/data2/morgante_lab/data/ukbiobank/genotypes/imputed/ukb22828_c1_b0_v3_s487271.sample",
              showProgress = FALSE, data.table = FALSE)
@@ -14,8 +14,9 @@ ids <- ids[-1, ]
 ###Select needed variables
 eigenvec <- eigenG[[2]][1:21] ##first 20 PCs
 rm(eigenG)
-dttt <- dttt[, c("ID", "PP0a", "DP0a", "SP0a", "AOP", "Sex_SI")]
-dat <- merge(dttt, eigenvec, by="ID", all.x=FALSE, all.y=FALSE, sort=FALSE)
+dtt <- dtt[, c("ID", "BMI1", "DP0a", "SP0a", "AOP", "Sex_SI")]
+dtt <- dtt[complete.cases(dtt), ]
+dat <- merge(dtt, eigenvec, by="ID", all.x=FALSE, all.y=FALSE, sort=FALSE)
 
 ###Subset individuals with imputed genotype data
 dat <- dat[which(as.character(dat$ID) %in% as.character(ids[,2])), ]
