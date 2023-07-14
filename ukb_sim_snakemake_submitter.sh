8c0ad323a80d88e38bcc32ae0f9e86096dd363da
fmorgante <fabiom@clemson.edu>
2023-07-14 07:57:44 -0400

    Add additional simulation scenario

--- ukb_sim_snakemake_submitter.sh
+++ ukb_sim_snakemake_submitter.sh
@@ -5,8 +5,8 @@
 #SBATCH --partition=compute
 #SBATCH --time=168:00:00
 #SBATCH --mem=2gb
-#SBATCH --output=run/log/ukb_caucasian_white_british_unrel_100000_blocks_shared_effects_indep_resid.%j.out
-#SBATCH --error=run/log/ukb_caucasian_white_british_unrel_100000_blocks_shared_effects_indep_resid.%j.err
+#SBATCH --output=run/log/ukb_caucasian_white_british_unrel_100000_equal_effects_low_pve_indep_resid.%j.out
+#SBATCH --error=run/log/ukb_caucasian_white_british_unrel_100000_equal_effects_low_pve_indep_resid.%j.err
 #SBATCH --mail-type=fail
 #SBATCH --mail-user=fabiom@clemson.edu
 
@@ -18,7 +18,8 @@ conda activate mr_mash_rss_proj
 #-p -n \
 ## test dag generation
 #snakemake -p -n -s ukb_sim_snakefile \
-#          --configfile ukb_sim_blocks_shared_effects_indep_resid.yaml
+#          --configfile ukb_sim_equal_effects_low_pve_indep_resid.yaml
+           #--configfile ukb_sim_blocks_shared_effects_indep_resid.yaml
            #--rerun-triggers mtime
            #--configfile ukb_sim_trait_1_only_effects_indep_resid.yaml \
            #--configfile ukb_sim_equal_effects_indep_resid.yaml \
@@ -28,7 +29,8 @@ snakemake \
   --profile slurm \
   --latency-wait 120 \
   -k \
-  --configfile ukb_sim_blocks_shared_effects_indep_resid.yaml \
-  --rerun-triggers mtime \
+  --configfile ukb_sim_equal_effects_low_pve_indep_resid.yaml \
+  #--rerun-triggers mtime
+  #--configfile ukb_sim_blocks_shared_effects_indep_resid.yaml
   #--configfile ukb_sim_trait_1_only_effects_indep_resid.yaml
   #--configfile ukb_sim_equal_effects_indep_resid.yaml
