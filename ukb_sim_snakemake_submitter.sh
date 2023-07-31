50cba6131ee74e7b80811fa665f944a3b8415275
fmorgante <fabiom@clemson.edu>
2023-07-31 11:48:08 -0400

    Update

--- ukb_sim_snakemake_submitter.sh
+++ ukb_sim_snakemake_submitter.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=ukb_sim_sm
 #SBATCH --ntasks=1
 #SBATCH --partition=compute
-#SBATCH --time=168:00:00
+#SBATCH --time=336:00:00
 #SBATCH --mem=2gb
-#SBATCH --output=run/log/ukb_caucasian_white_british_unrel_100000_equal_effects_low_pve_indep_resid.%j.out
-#SBATCH --error=run/log/ukb_caucasian_white_british_unrel_100000_equal_effects_low_pve_indep_resid.%j.err
+#SBATCH --output=run/log/ukb_caucasian_white_british_unrel_100000_trait_1_only_effects_indep_resid.%j.out
+#SBATCH --error=run/log/ukb_caucasian_white_british_unrel_100000_trait_1_only_effects_indep_resid.%j.err
 #SBATCH --mail-type=fail
 #SBATCH --mail-user=fabiom@clemson.edu
 
@@ -18,19 +18,22 @@ conda activate mr_mash_rss_proj
 #-p -n \
 ## test dag generation
 #snakemake -p -n -s ukb_sim_snakefile \
-#          --configfile ukb_sim_equal_effects_low_pve_indep_resid.yaml
+#          --rerun-triggers mtime \
+#          --configfile ukb_sim_trait_1_only_effects_indep_resid.yaml
            #--configfile ukb_sim_blocks_shared_effects_indep_resid.yaml
-           #--rerun-triggers mtime
-           #--configfile ukb_sim_trait_1_only_effects_indep_resid.yaml \
-           #--configfile ukb_sim_equal_effects_indep_resid.yaml \
+           #--configfile ukb_sim_equal_effects_indep_resid.yaml
+           #--configfile ukb_sim_equal_effects_10traits_indep_resid.yaml
+           #--configfile ukb_sim_equal_effects_low_pve_indep_resid.yaml
 
 snakemake \
   -s ukb_sim_snakefile \
   --profile slurm \
   --latency-wait 120 \
   -k \
-  --configfile ukb_sim_equal_effects_low_pve_indep_resid.yaml \
-  #--rerun-triggers mtime
+  --rerun-triggers mtime \
+  --configfile ukb_sim_trait_1_only_effects_indep_resid.yaml
   #--configfile ukb_sim_blocks_shared_effects_indep_resid.yaml
-  #--configfile ukb_sim_trait_1_only_effects_indep_resid.yaml
   #--configfile ukb_sim_equal_effects_indep_resid.yaml
+  #--configfile ukb_sim_equal_effects_10traits_indep_resid.yaml
+  #--configfile ukb_sim_equal_effects_low_pve_indep_resid.yaml
+  #--configfile ukb_sim_blocks_shared_effects_indep_resid.yaml
