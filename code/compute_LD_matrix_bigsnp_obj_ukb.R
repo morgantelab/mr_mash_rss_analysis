cb08657d9a704d068c785a24811fc6dc1f209de6
fmorgante <fabiom@clemson.edu>
2023-08-21 11:12:56 -0400

    Fiz a couple of issues

--- code/compute_LD_matrix_bigsnp_obj_ukb.R
+++ code/compute_LD_matrix_bigsnp_obj_ukb.R
@@ -6,6 +6,7 @@ library(bigsnpr)
 parser <- OptionParser()
 parser <- add_option(parser, c("--geno"), type="character")
 parser <- add_option(parser, c("--genetic_map"), type="character", default=NULL)
+parser <- add_option(parser, c("--samples_to_remove"), type="character", default=NULL)
 parser <- add_option(parser, c("--chr"), type="integer")
 parser <- add_option(parser, c("--seed"), type="integer")
 parser <- add_option(parser, c("--ncores"), type="integer")
@@ -18,6 +19,7 @@ outparse <- parse_args(parser)
 
 geno_dat <- outparse$geno
 genetic_map <- outparse$genetic_map
+samples <- outparse$samples_to_remove
 seed <- outparse$seed
 chr <- outparse$chr
 ncores <- outparse$ncores
@@ -40,6 +42,17 @@ geno <- snp_attach(geno_dat)
 
 p <- geno$genotypes$ncol
 
+n <- geno$genotypes$nrow
+
+samples_to_remove <- tryCatch(data.table::fread(samples, data.table=FALSE, colClasses = "character", header=FALSE), 
+                    error = function(e) {
+                      return(NULL)
+                    },
+                    warning = function(w) {
+                      return(NULL)
+                    }
+)
+
 ###Impute missing geno1types
 if(impute_missing){
   X <- snp_fastImputeSimple(geno$genotypes, method = "mean2", ncores = ncores)
@@ -56,10 +69,36 @@ if(window_size==0){
 
 ###Set chromosome
 if(chr==0){
-  inds <- seq_len(p)
+  col_inds <- seq_len(p)
 } else {
-  inds <- which(geno$map$chromosome==chr)
+  col_inds <- which(geno$map$chromosome==chr)
 }
+
+cat(sprintf("After optional filtering, %d variants remain.\n", length(col_inds)))
+
+###Subset individuals (NB this only works with the UKB data)
+if(!is.null(samples_to_remove)){
+  if(ncol(samples_to_remove) > 1){
+    samples_to_remove <- samples_to_remove[, 2]
+  }
+  
+  ukb_data_location <- "/data2/morgante_lab/data/ukbiobank/genotypes/imputed/"
+  ids <- bigreadr::fread2(paste0(ukb_data_location, "ukb22828_c1_b0_v3_s487271.sample"))
+  ids <- ids[-1, ]
+  
+  ids1 <- data.table::fread("../data/misc/ukb_cleaned_bc_ind_ids.txt", data.table=FALSE, 
+                            colClasses = "character", header=FALSE)
+  
+  #Keep only bc selected individuals
+  ids_sel <- as.character(ids[as.character(ids$ID_2) %in% ids1[,2], 2])
+  
+  #Keep only training individuals  
+  row_inds <- which(!(ids_sel %in% samples_to_remove))
+} else {
+  row_inds <- seq_len(n)
+}
+
+cat(sprintf("After optional filtering, %d individuals remain.\n", length(row_inds)))
 
 ###Compute LD
 if(sparse){
@@ -71,9 +110,9 @@ if(sparse){
     POS <- geno$map$physical.pos
   }
   
-  LD <- snp_cor(Gna=X, ind.col=inds, size=win, infos.pos=POS[inds], thr_r2=thr_r2, ncores=ncores)
+  LD <- snp_cor(Gna=X, ind.col=col_inds, ind.row=row_inds, size=win, infos.pos=POS[col_inds], thr_r2=thr_r2, ncores=ncores)
 } else {
-  LD <- snp_cor(Gna=X, ind.col=inds, size=win, thr_r2=thr_r2, ncores=ncores)
+  LD <- snp_cor(Gna=X, ind.col=col_inds, ind.row=row_inds, size=win, thr_r2=thr_r2, ncores=ncores)
 }
   
 ###Save to binary file    
