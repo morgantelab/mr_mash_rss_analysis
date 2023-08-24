f4d8b1e270cb3356144eaa89ebd79fa501de388a
fmorgante <fabiom@clemson.edu>
2023-08-24 10:03:39 -0400

    Match real data analysis

--- code/compute_data_driven_cov_all_chr_ukb.R
+++ code/compute_data_driven_cov_all_chr_ukb.R
@@ -20,6 +20,7 @@ parser <- add_option(parser, c("--n_PCs"), type="integer")
 parser <- add_option(parser, c("--flash_remove_singleton"), type="logical")
 parser <- add_option(parser, c("--ED_algorithm"), type="character")
 parser <- add_option(parser, c("--ted_zero_thresh"), type="numeric", default=1e-10)
+parser <- add_option(parser, c("--canonical_cov"), type="logical")
 parser <- add_option(parser, c("--seed"), type="integer")
 outparse <- parse_args(parser)
 
@@ -33,6 +34,7 @@ npcs <- outparse$n_PCs
 flash_remove_singleton <- outparse$flash_remove_singleton
 ED_algorithm <- outparse$ED_algorithm
 ted_zero_thresh <- outparse$ted_zero_thresh
+canonical_cov <- outparse$canonical_cov
 seed <- outparse$seed
 
 ###Set seed
@@ -53,11 +55,9 @@ for(i in chrs){
   strong <- which(apply(Z, 1, is_strong, strong_thresh))
   
   if(i==1){
-    Bhat_strong <- dat$Bhat[strong, ]
-    Shat_strong <- dat$Shat[strong, ]
+    Z_strong <- Z[strong, ]
   } else {
-    Bhat_strong <- rbind(Bhat_strong, dat$Bhat[strong, ])
-    Shat_strong <- rbind(Shat_strong, dat$Shat[strong, ])
+    Z_strong <- rbind(Z_strong, Z[strong, ])
   }
 }
 
@@ -72,16 +72,16 @@ if(residual_cov=="diagonal"){
   V <- readRDS(residual_cov)
 }
 
-dat_mash <- mash_set_data(Bhat_strong, Shat_strong, V=cov2cor(V))
+dat_mash <- mash_set_data(Z_strong, V=cov2cor(V))
 
 if(ED_algorithm=="bovy"){
   ##Compute factor analyses
   U_pca <- cov_pca(data=dat_mash, npc=npcs)
   U_flash_default <- cov_flash(data=dat_mash, factors="default", tag="default",
                                remove_singleton=flash_remove_singleton, output_model=NULL)
-  U_flash_nonneg <- cov_flash(data=dat_mash, factors="nonneg", tag="nonneg",
-                              remove_singleton=flash_remove_singleton, output_model=NULL)
-  U_emp <- mr.mash.alpha:::cov_empirical(data=dat_mash)
+  # U_flash_nonneg <- cov_flash(data=dat_mash, factors="nonneg", tag="nonneg",
+  #                             remove_singleton=flash_remove_singleton, output_model=NULL)
+  U_emp <- crossprod(Z_strong) / nrow(Z_strong)
 
   ##De-noise data-driven matrices via extreme deconvolution
   U_datadriven <- c(list(BB=U_emp), U_pca)
@@ -90,10 +90,15 @@ if(ED_algorithm=="bovy"){
     U_datadriven <- c(U_datadriven, U_flash_default)
   }
 
-  if(!is.null(U_flash_nonneg)){
-    U_datadriven <- c(U_datadriven, U_flash_nonneg)
+  # if(!is.null(U_flash_nonneg)){
+  #   U_datadriven <- c(U_datadriven, U_flash_nonneg)
+  # }
+  
+  if(canonical_cov){
+    U_can <- cov_canonical(dat_mash)
+    U_datadriven <- c(U_datadriven, U_can)
   }
-
+  
   U_ed <- cov_ed(dat_mash, U_datadriven)
 } else if(ED_algorithm=="ted"){
   library(udr)
