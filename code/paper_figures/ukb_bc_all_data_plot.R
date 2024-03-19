1ddce4c1a47f3646a4a51cae82118ca21f6353c6
fmorgante <fabiom@clemson.edu>
2024-03-19 16:36:49 -0400

    Add plot of change in R2 vs h2g

--- code/paper_figures/ukb_bc_all_data_plot.R
+++ code/paper_figures/ukb_bc_all_data_plot.R
@@ -1,4 +1,5 @@
 ###Load libraries
+library(dplyr)
 library(ggplot2)
 library(cowplot)
 
@@ -8,9 +9,10 @@ cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
 
 
 ###Full data
-res_full <- readRDS("../output/paper_figures_intermediate_data/ukb_bc_full_data.rds")
+##Prediction accuracy
+res_full1 <- readRDS("../output/paper_figures_intermediate_data/ukb_bc_full_data.rds")
 
-res_full <- transform(res_full,
+res_full <- transform(res_full1,
                  method=factor(method, levels=c("bayesR", "ldpred2_auto", 
                                                 "mr_mash_rss_sparse_LD_V_all_chr_bayesR_init_prior_finemapped"),
                                labels=c("SBayesR", "LDpred2", "mr.mash-rss")),
@@ -38,6 +40,48 @@ p_3 <- ggplot(res_full, aes(x = trait, y = score, fill = method)) +
 
 ggsave("../analysis/paper_figures/Fig3.pdf", plot=p_3, device="pdf", units="in", height=9, width=11)
 
+###Percentage increase in R2 vs h2g
+h2_dat <- readRDS("../output/paper_figures_intermediate_data/ukb_bc_full_data_h2.rds")
+means_full <- res_full1 %>% group_by(method, trait) %>% summarise(avg = mean(score)) %>% as.data.frame(.)
+means_full_bayesR <- means_full[which(means_full$method=="bayesR"), ]
+means_full_LDpred2 <- means_full[which(means_full$method=="ldpred2_auto"), ]
+means_full_mrmashrss <- means_full[which(means_full$method=="mr_mash_rss_sparse_LD_V_all_chr_bayesR_init_prior_finemapped"), ]
+
+change_bayesR_mrmashrss_full <- ((means_full_mrmashrss$avg - means_full_bayesR$avg)/means_full_bayesR$avg)*100
+names(change_bayesR_mrmashrss_full) <- means_full_mrmashrss$trait 
+change_LDpred2_mrmashrss_full <- ((means_full_mrmashrss$avg - means_full_LDpred2$avg)/means_full_LDpred2$avg)*100
+names(change_LDpred2_mrmashrss_full) <- means_full_mrmashrss$trait
+
+dat_change_LDpred2_mrmashrss_full <- data.frame(trait=names(change_LDpred2_mrmashrss_full), perc_change=change_LDpred2_mrmashrss_full)
+dat_LDpred2_full <- inner_join(dat_change_LDpred2_mrmashrss_full, h2_dat, by=join_by(trait))
+dat_change_bayesR_mrmashrss_full <- data.frame(trait=names(change_bayesR_mrmashrss_full), perc_change=change_bayesR_mrmashrss_full)
+dat_bayesR_full <- inner_join(dat_change_bayesR_mrmashrss_full, h2_dat, by=join_by(trait))
+
+###Plot of relative improvement of mr.mash over the enet based on RMSE vs sample size by tissue
+p_bayesR <- ggplot(dat_bayesR_full, aes(x=h2, y=perc_change)) + 
+  geom_point(shape=16) +
+  geom_smooth(method='lm', formula= y~x) +
+  labs(x = expression(h["g"]^2), y = expression("Relative difference in" ~ italic(R)^2), title="mr.mash-rss vs SBayesR") + 
+  theme_cowplot(font_size = 14) +
+  theme(plot.title = element_text(hjust = 0.5, size=14))
+
+p_LDpred2 <- ggplot(dat_LDpred2_full, aes(x=h2, y=perc_change)) + 
+  geom_point(shape=16) +
+  geom_smooth(method='lm', formula= y~x) +
+  labs(x = expression(h["g"]^2), y = expression("Relative difference in" ~ italic(R)^2), title="mr.mash-rss vs LDpred2") + 
+  theme_cowplot(font_size = 14) +
+  theme(plot.title = element_text(hjust = 0.5, size=14))
+
+p_4 <- plot_grid(p_LDpred2, p_bayesR,
+                 align = 'vh',
+                 labels = c("A", "B"),
+                 hjust = -1,
+                 nrow = 1
+)
+
+#print(p_4)
+
+ggsave("../analysis/paper_figures/Fig4.pdf", plot=p_4, device="pdf", units="in", height=5, width=11)
 
 ###Sampled data
 res_sampled <- readRDS("../output/paper_figures_intermediate_data/ukb_bc_sampled_data.rds")
@@ -55,7 +99,7 @@ res_sampled <- transform(res_sampled,
                                        "PLT#", "PCT", "PDW", 
                                        "WBC#", "LYMPH%", "MONO%", "NEUT%", "EO%", "BASO%")))
 
-p_4 <- ggplot(res_sampled, aes(x = trait, y = score, fill = method)) +
+p_5 <- ggplot(res_sampled, aes(x = trait, y = score, fill = method)) +
   geom_boxplot(color = "black", outlier.size = 0.5, width = 0.75) +
   # stat_summary(fun=mean, geom="point", shape=23,
   #              position = position_dodge2(width = 0.87,
@@ -66,7 +110,6 @@ p_4 <- ggplot(res_sampled, aes(x = trait, y = score, fill = method)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
         legend.position=c(0.8,0.9))
 
-#print(p_4)
+#print(p_5)
 
-ggsave("../analysis/paper_figures/Fig4.pdf", plot=p_4, device="pdf", units="in", height=9, width=11)
-
+ggsave("../analysis/paper_figures/Fig5.pdf", plot=p_5, device="pdf", units="in", height=9, width=11)
--- code/paper_figures/ukb_bc_all_data_prep.R
+++ code/paper_figures/ukb_bc_all_data_prep.R
@@ -11,6 +11,7 @@ n_col <- 5
 n_row <- length(repz) * length(methodz) * 16
 
 ###Full data
+##Prediction accuracy
 pheno_full <- readRDS("../data/phenotypes/ukb_cleaned_bc_adjusted_pheno_test_1.rds")
 
 res_full <- as.data.frame(matrix(NA, ncol=n_col, nrow=n_row))
@@ -35,6 +36,19 @@ for(repp in repz){
 }
 
 saveRDS(res_full, "../output/paper_figures_intermediate_data/ukb_bc_full_data.rds")
+
+##Genomic heritability
+dat_ldsc <- matrix(as.numeric(NA), 5, 16)
+
+for(i in 1:5){
+  for(s in 1:16){
+    dat_ldsc[i, s] <- readRDS(paste0("../output/ldsc_fit/ukb_bc_chrAll_ldsc_fit_trait", s, "_", i, ".rds"))["h2"]
+  }
+}
+
+h2_dat <- data.frame(trait=colnames(pheno_full), h2=colMeans(dat_ldsc))
+
+saveRDS(h2_dat, "../output/paper_figures_intermediate_data/ukb_bc_full_data_h2.rds")
 
 
 ###Sampled data
